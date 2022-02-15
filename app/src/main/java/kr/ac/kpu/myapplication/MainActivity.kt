8bd5f6d532416a0becc080b15473c1f7acd15281
0sik <ghdtlrdudtlr@naver.com>
2022-02-15 18:56:03 +0900

    2장 과제

--- app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
@@ -9,21 +9,22 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var button1: Button
     lateinit var button2: Button
+    lateinit var button3: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button1=findViewById(R.id.button1)
         button1.setOnClickListener {
-            Toast.makeText(applicationContext,"버튼을 눌렀어요", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext,"학과:컴퓨터공학부(소프트웨어전공)", Toast.LENGTH_SHORT).show()
         }
-        button2=findViewById(R.id.button1)
+        button2=findViewById(R.id.button2)
         button2.setOnClickListener {
-            Toast.makeText(applicationContext, "버튼2가 눌렸어요", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "이름:홍길동", Toast.LENGTH_SHORT).show()
         }
-        button2=findViewById(R.id.button1)
-        button2.setOnClickListener {
-            Toast.makeText(applicationContext, "버튼2가 눌렸어요", Toast.LENGTH_SHORT).show()
+        button3=findViewById(R.id.button3)
+        button3.setOnClickListener {
+            Toast.makeText(applicationContext, "학번:2000202020", Toast.LENGTH_SHORT).show()
         }
     }
 }
