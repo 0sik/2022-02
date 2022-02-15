ebecb04ea255a4e20f7843cce0f7697076e85636
0sik <ghdtlrdudtlr@naver.com>
2022-02-15 20:24:30 +0900

    2장 2-3 실습

--- app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package kr.ac.kpu.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,21 +12,29 @@ class MainActivity : AppCompatActivity() {
     lateinit var button1: Button
     lateinit var button2: Button
     lateinit var button3: Button
+    lateinit var button4: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button1=findViewById(R.id.button1)
         button1.setOnClickListener {
-            Toast.makeText(applicationContext,"학과:컴퓨터공학부(소프트웨어전공)", Toast.LENGTH_SHORT).show()
+            val mlntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com"))
+            startActivity(mlntent)
         }
         button2=findViewById(R.id.button2)
         button2.setOnClickListener {
-            Toast.makeText(applicationContext, "이름:홍길동", Toast.LENGTH_SHORT).show()
+            val mlntent = Intent(Intent.ACTION_VIEW,Uri.parse("tel:/911"))
+            startActivity(mlntent)
         }
         button3=findViewById(R.id.button3)
         button3.setOnClickListener {
-            Toast.makeText(applicationContext, "학번:2000202020", Toast.LENGTH_SHORT).show()
+            val mlntent = Intent(Intent.ACTION_VIEW,Uri.parse("content://media/internal/images/media"))
+            startActivity(mlntent)
+        }
+        button4=findViewById(R.id.button4)
+        button4.setOnClickListener {
+           finish()
         }
     }
 }
