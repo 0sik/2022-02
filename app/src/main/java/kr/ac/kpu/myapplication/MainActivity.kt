1c314cc824b62220b7b700f2ff2af62c68d4f90a
0sik <ghdtlrdudtlr@naver.com>
2022-02-20 18:52:32 +0900

    4-1장 4-3 직접풀어보기

--- app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
@@ -1,40 +1,97 @@
-package kr.ac.kpu.myapplication
+package com.cookandroid.project4_1
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var button1: Button
-    lateinit var button2: Button
-    lateinit var button3: Button
-    lateinit var button4: Button
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var Edit1: EditText? = null
+    var Edit2: EditText? = null
+    var btnAdd: Button? = null
+    var btnSub: Button? = null
+    var btnMul: Button? = null
+    var btnDiv: Button? = null
+    var btnExt: Button? = null
+    var textResult: TextView? = null
+    var num1: String? = null
+    var num2: String? = null
+    var result: Float? = null
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1=findViewById(R.id.button1)
-        button1.setOnClickListener {
-            val mlntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com"))
-            startActivity(mlntent)
+        title = "초간단 계산기(수정)"
+        Edit1 = findViewById<View>(R.id.Edit1) as EditText
+        Edit2 = findViewById<View>(R.id.Edit2) as EditText
+        btnAdd = findViewById<View>(R.id.BtnAdd) as Button
+        btnSub = findViewById<View>(R.id.BtnSub) as Button
+        btnMul = findViewById<View>(R.id.BtnMul) as Button
+        btnDiv = findViewById<View>(R.id.BtnDiv) as Button
+        btnExt = findViewById<View>(R.id.BtnExt) as Button
+        textResult = findViewById<View>(R.id.TextResult) as TextView
+        btnAdd!!.setOnClickListener {
+            num1 = Edit1!!.text.toString()
+            num2 = Edit2!!.text.toString()
+            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
+                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                myToast.show()
+            } else {
+                result = num1!!.toFloat() + num2!!.toFloat()
+                textResult!!.text = "계산 결과 : " + result.toString()
+            }
         }
-        button2=findViewById(R.id.button2)
-        button2.setOnClickListener {
-            val mlntent = Intent(Intent.ACTION_VIEW,Uri.parse("tel:/911"))
-            startActivity(mlntent)
+        btnSub!!.setOnClickListener {
+            num1 = Edit1!!.text.toString()
+            num2 = Edit2!!.text.toString()
+            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
+                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                myToast.show()
+            } else {
+                result = num1!!.toFloat() - num2!!.toFloat()
+                textResult!!.text = "계산 결과 : " + result.toString()
+            }
         }
-        button3=findViewById(R.id.button3)
-        button3.setOnClickListener {
-            val mlntent = Intent(Intent.ACTION_VIEW,Uri.parse("content://media/internal/images/media"))
-            startActivity(mlntent)
+        btnMul!!.setOnClickListener {
+            num1 = Edit1!!.text.toString()
+            num2 = Edit2!!.text.toString()
+            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
+                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                myToast.show()
+            } else {
+                result = num1!!.toFloat() * num2!!.toFloat()
+                textResult!!.text = "계산 결과 : " + result.toString()
+            }
         }
-        button4=findViewById(R.id.button4)
-        button4.setOnClickListener {
-           finish()
+        btnDiv!!.setOnClickListener {
+            num1 = Edit1!!.text.toString()
+            num2 = Edit2!!.text.toString()
+            val zero = "0"
+            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
+                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                myToast.show()
+            } else if (num2 == zero) {
+                val myToast =
+                    Toast.makeText(applicationContext, "0으로 나눌 수 없습니다.", Toast.LENGTH_SHORT)
+                myToast.show()
+            } else {
+                result = num1!!.toFloat() / num2!!.toFloat()
+                textResult!!.text = "계산 결과 : " + result.toString()
+            }
+        }
+        btnExt!!.setOnClickListener {
+            num1 = Edit1!!.text.toString()
+            num2 = Edit2!!.text.toString()
+            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
+                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
+                myToast.show()
+            } else {
+                result = num1!!.toFloat() % num2!!.toFloat()
+                textResult!!.text = "계산 결과 : " + result.toString()
+            }
         }
     }
 }
