ff4e81028f660ca1a314324e347b5766281094c8
0sik <ghdtlrdudtlr@naver.com>
2022-03-07 18:58:23 +0900

    실습4-1 최종본

--- app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
@@ -1,96 +1,73 @@
+package kr.ac.kpu.myapplication
 
-import android.R
+
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 
+
+
+
 class MainActivity : AppCompatActivity() {
-    var Edit1: EditText? = null
-    var Edit2: EditText? = null
-    var btnAdd: Button? = null
-    var btnSub: Button? = null
-    var btnMul: Button? = null
-    var btnDiv: Button? = null
-    var btnExt: Button? = null
-    var textResult: TextView? = null
-    var num1: String? = null
-    var num2: String? = null
-    var result: Float? = null
-    public override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var edit1: EditText;
+    lateinit var edit2: EditText
+    lateinit var btnAdd: Button;
+    lateinit var btnSub: Button
+    lateinit var btnMul: Button;
+    lateinit var btnDiv: Button
+    lateinit var textResult: TextView
+    lateinit var num1: String;
+    lateinit var num2: String
+    var result: Int? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "초간단 계산기(수정)"
-        Edit1 = findViewById<View>(R.id.Edit1) as EditText
-        Edit2 = findViewById<View>(R.id.Edit2) as EditText
-        btnAdd = findViewById<View>(R.id.BtnAdd) as Button
-        btnSub = findViewById<View>(R.id.BtnSub) as Button
-        btnMul = findViewById<View>(R.id.BtnMul) as Button
-        btnDiv = findViewById<View>(R.id.BtnDiv) as Button
-        btnExt = findViewById<View>(R.id.BtnExt) as Button
-        textResult = findViewById<View>(R.id.TextResult) as TextView
-        btnAdd!!.setOnClickListener {
-            num1 = Edit1!!.text.toString()
-            num2 = Edit2!!.text.toString()
-            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
-                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
-                myToast.show()
-            } else {
-                result = num1!!.toFloat() + num2!!.toFloat()
-                textResult!!.text = "계산 결과 : " + result.toString()
-            }
+        title = "초간단 계산기"
+
+        edit1 = findViewById<EditText>(R.id.Edit1)
+        edit2 = findViewById<EditText>(R.id.Edit2)
+        btnAdd = findViewById<Button>(R.id.BtnAdd)
+        btnSub = findViewById<Button>(R.id.BtnSub)
+        btnMul = findViewById<Button>(R.id.BtnMul)
+        btnDiv = findViewById<Button>(R.id.BtnDiv)
+        textResult = findViewById<TextView>(R.id.TextResult)
+
+        btnAdd.setOnTouchListener { view, motionEvent ->
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) + Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result.toString()
+            false
         }
-        btnSub!!.setOnClickListener {
-            num1 = Edit1!!.text.toString()
-            num2 = Edit2!!.text.toString()
-            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
-                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
-                myToast.show()
-            } else {
-                result = num1!!.toFloat() - num2!!.toFloat()
-                textResult!!.text = "계산 결과 : " + result.toString()
-            }
+
+        btnSub.setOnTouchListener { view, motionEvent ->
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) - Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result.toString()
+            false
         }
-        btnMul!!.setOnClickListener {
-            num1 = Edit1!!.text.toString()
-            num2 = Edit2!!.text.toString()
-            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
-                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
-                myToast.show()
-            } else {
-                result = num1!!.toFloat() * num2!!.toFloat()
-                textResult!!.text = "계산 결과 : " + result.toString()
-            }
+        btnMul.setOnTouchListener { view, motionEvent ->
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) * Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result.toString()
+            false
         }
-        btnDiv!!.setOnClickListener {
-            num1 = Edit1!!.text.toString()
-            num2 = Edit2!!.text.toString()
-            val zero = "0"
-            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
-                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
-                myToast.show()
-            } else if (num2 == zero) {
-                val myToast =
-                    Toast.makeText(applicationContext, "0으로 나눌 수 없습니다.", Toast.LENGTH_SHORT)
-                myToast.show()
-            } else {
-                result = num1!!.toFloat() / num2!!.toFloat()
-                textResult!!.text = "계산 결과 : " + result.toString()
-            }
+        btnDiv.setOnTouchListener { view, motionEvent ->
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) / Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result.toString()
+            false
         }
-        btnExt!!.setOnClickListener {
-            num1 = Edit1!!.text.toString()
-            num2 = Edit2!!.text.toString()
-            if (num1!!.trim { it <= ' ' } == "" || num2!!.trim { it <= ' ' } == "") {
-                val myToast = Toast.makeText(applicationContext, "입력 값이 비었습니다.", Toast.LENGTH_SHORT)
-                myToast.show()
-            } else {
-                result = num1!!.toFloat() % num2!!.toFloat()
-                textResult!!.text = "계산 결과 : " + result.toString()
-            }
-        }
+
     }
 }
