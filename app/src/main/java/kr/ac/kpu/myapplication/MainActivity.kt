3fa39a11961bcbe2e0aac3488ea897104e1706dd
0sik <ghdtlrdudtlr@naver.com>
2022-03-10 15:28:28 +0900

    4장 과제

--- app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
@@ -13,16 +13,17 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var edit1: EditText;
-    lateinit var edit2: EditText
+    lateinit var num1: EditText;
+    lateinit var num2: EditText
     lateinit var btnAdd: Button;
     lateinit var btnSub: Button
     lateinit var btnMul: Button;
     lateinit var btnDiv: Button
-    lateinit var textResult: TextView
-    lateinit var num1: String;
-    lateinit var num2: String
-    var result: Int? = null
+    lateinit var result: TextView
+    lateinit var giho: TextView
+    lateinit var Num1: String;
+    lateinit var Num2: String
+    var result2: Int? = null
 
 
 
@@ -31,41 +32,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         title = "초간단 계산기"
 
-        edit1 = findViewById<EditText>(R.id.Edit1)
-        edit2 = findViewById<EditText>(R.id.Edit2)
+        num1 = findViewById<EditText>(R.id.num1)
+        num2 = findViewById<EditText>(R.id.num2)
         btnAdd = findViewById<Button>(R.id.BtnAdd)
         btnSub = findViewById<Button>(R.id.BtnSub)
         btnMul = findViewById<Button>(R.id.BtnMul)
         btnDiv = findViewById<Button>(R.id.BtnDiv)
-        textResult = findViewById<TextView>(R.id.TextResult)
+        result = findViewById<TextView>(R.id.result)
+        giho = findViewById<TextView>(R.id.giho)
 
         btnAdd.setOnTouchListener { view, motionEvent ->
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            result = Integer.parseInt(num1) + Integer.parseInt(num2)
-            textResult.text = "계산 결과 : " + result.toString()
+            Num1 = num1.text.toString()
+            Num2 = num2.text.toString()
+            result2 = Integer.parseInt(Num1) + Integer.parseInt(Num2)
+            giho.text ="+"
+            result.text = "계산 결과 : " + result2.toString()
             false
         }
 
         btnSub.setOnTouchListener { view, motionEvent ->
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            result = Integer.parseInt(num1) - Integer.parseInt(num2)
-            textResult.text = "계산 결과 : " + result.toString()
+            Num1 = num1.text.toString()
+            Num2 = num2.text.toString()
+            result2 = Integer.parseInt(Num1) - Integer.parseInt(Num2)
+            giho.text ="-"
+            result.text = "계산 결과 : " + result2.toString()
             false
         }
+
         btnMul.setOnTouchListener { view, motionEvent ->
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            result = Integer.parseInt(num1) * Integer.parseInt(num2)
-            textResult.text = "계산 결과 : " + result.toString()
+            Num1 = num1.text.toString()
+            Num2 = num2.text.toString()
+            result2 = Integer.parseInt(Num1) * Integer.parseInt(Num2)
+            giho.text ="*"
+            result.text = "계산 결과 : " + result2.toString()
             false
         }
         btnDiv.setOnTouchListener { view, motionEvent ->
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            result = Integer.parseInt(num1) / Integer.parseInt(num2)
-            textResult.text = "계산 결과 : " + result.toString()
+            Num1 = num1.text.toString()
+            Num2 = num2.text.toString()
+            result2 = Integer.parseInt(Num1) / Integer.parseInt(Num2)
+            giho.text ="/"
+            result.text = "계산 결과 : " + result2.toString()
             false
         }
 
