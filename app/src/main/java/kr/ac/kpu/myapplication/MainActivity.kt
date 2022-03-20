aaa02caf6e22de39a2b69851b6ee4d729a2d0e2e
0sik <ghdtlrdudtlr@naver.com>
2022-03-20 17:08:45 +0900

    직접풀어보기 5-2

--- app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/myapplication/MainActivity.kt
@@ -2,77 +2,19 @@ package kr.ac.kpu.myapplication
 
 
 import android.os.Bundle
-import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var text1 : TextView
-    lateinit var text2 : TextView
-    lateinit var chkswit : Switch
-    lateinit var rGroup1 : RadioGroup
-    lateinit var rdoDog : RadioButton
-    lateinit var rdocat : RadioButton
-    lateinit var rdorabbit : RadioButton
-    lateinit var imgpet : ImageView
-    lateinit var finish : Button
-    lateinit var first : Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "애완동물 사진 보기"
-        text1 = findViewById(R.id.Text1)
-        text2 = findViewById(R.id.Text2)
-        chkswit = findViewById(R.id.ChkSwit)
-        rGroup1 = findViewById(R.id.Rgroup1)
-        rdoDog = findViewById(R.id.RdoDog)
-        rdocat = findViewById(R.id.RdoCat)
-        rdorabbit = findViewById(R.id.RdoRabbit)
-        imgpet = findViewById(R.id.Imgpet)
-        first = findViewById(R.id.first)
-        finish = findViewById(R.id.finish)
 
-        chkswit.setOnCheckedChangeListener { compoundButton, b ->
-            if (chkswit.isChecked) {
-                text2.visibility = android.view.View.VISIBLE
-                rGroup1.visibility = android.view.View.VISIBLE
-                first.visibility = android.view.View.VISIBLE
-                finish.visibility = android.view.View.VISIBLE
-                imgpet.visibility = android.view.View.VISIBLE
-            } else {
-                text2.visibility = android.view.View.INVISIBLE
-                rGroup1.visibility = android.view.View.INVISIBLE
-                first.visibility = android.view.View.INVISIBLE
-                finish.visibility = android.view.View.INVISIBLE
-                imgpet.visibility = android.view.View.INVISIBLE
-            }
-            finish.setOnClickListener { finish() }
-            first.setOnClickListener {
-                chkswit.setChecked(false)
-                text2.visibility = android.view.View.INVISIBLE
-                rGroup1.visibility = android.view.View.INVISIBLE
-                first.visibility = android.view.View.INVISIBLE
-                finish.visibility = android.view.View.INVISIBLE
-                imgpet.visibility = android.view.View.INVISIBLE
-            }
-
-            rGroup1.setOnCheckedChangeListener { radioGroup, i ->
-                when(rGroup1.checkedRadioButtonId){
-                    R.id.RdoDog->imgpet.setImageResource(R.drawable.dog)
-                    R.id.RdoCat->imgpet.setImageResource(R.drawable.cat)
-                    R.id.RdoRabbit->imgpet.setImageResource(R.drawable.rabbit)
-                    else -> Toast.makeText(applicationContext,"동물 먼저 선택하세요",Toast.LENGTH_SHORT).show()
-                }
-            }
-
-
-
-            }
-        }
     }
+}
 
 
